6c46d61aaa806a7409c17901770d4232409a41c7
GabryelXavier <78799621+GabryelXavier@users.noreply.github.com>
2023-01-22 02:46:36 -0300

    Api funcional
    Api funcional em memória

---
+++ GGastosApi/Controllers/CompraController.cs
@@ -0,0 +1,40 @@
+﻿using GGastosApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GGastosApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CompraController : ControllerBase
+{
+
+    private static List<Compra> compras = new List<Compra>();
+
+    [HttpPost]
+    public IActionResult AdicionaCompra([FromBody] Compra compra)
+    {
+
+        compras.Add(compra);
+        return CreatedAtAction(nameof(RecuperarCompraPorId), new { id = compra.IdCompra }, compra);
+    }
+
+    [HttpGet]
+    public IEnumerable<Compra> RecuperarCompras([FromQuery] int skip, [FromQuery] int take)
+    {
+
+        return compras.Skip(skip).Take(take);
+
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperarCompraPorId(int id)
+    {
+
+        var compra =  compras.FirstOrDefault(compra => compra.IdCompra == id );
+
+        if (compra == null) return NotFound();
+        return Ok(compra);
+
+    }
+
+}
---
+++ GGastosApi/Models/Compra.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GGastosApi.Models;
+
+public class Compra
+{
+
+    private static int id = 0;
+
+    public int IdCompra { get; }
+
+    public Compra()
+    {
+        id++;
+        this.IdCompra = id;
+    }
+
+    [Required(ErrorMessage = "Campo Local é obrigatório")]
+    [MaxLength(50, ErrorMessage = "Campo Local precisa ter no máximo 50 caracteres")]
+    public string Local { get; set; }
+    [Required(ErrorMessage = "Campo Descrição é obrigatório")]
+    [MaxLength(100, ErrorMessage = "Campo descrição precisa ter no máximo 100 caracteres")]
+    public string Descricao { get; set; }
+
+    [Required(ErrorMessage = "Campo Valor é obrigatório")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Campo valor precisa ser maior que 0")]
+    public double Valor { get; set; }
+
+    private DateTime dataInsercao = DateTime.Now;
+
+    public DateTime DataInsercao 
+    {
+        get { return this.dataInsercao; }
+     
+    }
+}
